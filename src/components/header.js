b7f25822dab8f1dd29026b1e746b3e88959fedca
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-08-20 00:52:35 -0700

    add eslint, prettier, pretty-quick, lint-staged, husky

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,37 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem'
-    }}>
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
     <div
       style={{
-        margin: '0 auto',
+        margin: `0 auto`,
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}>
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}>
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
           {siteTitle}
         </Link>
       </h1>
     </div>
   </div>
 )
+Header.propTypes = {
+  siteTitle: PropTypes.string.isRequired,
+}
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,29 +10,32 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' }
+        { name: `description`, content: `Sample` },
+        { name: `keywords`, content: `sample, something` },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
     <div
       style={{
-        margin: '0 auto',
+        margin: `0 auto`,
         maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}>
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }}
+    >
       {children()}
     </div>
   </div>
 )
 
 Layout.propTypes = {
-  children: PropTypes.func
+  children: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
 }
 
 export default Layout
 
+/* eslint-disable no-undef */
 export const query = graphql`
   query SiteTitleQuery {
     site {
