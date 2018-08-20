7e52f7c10426a95070008709c67f3c5ab52fa556
Reuben Reyes <reuben.a.reyes@gmail.com>
2018-08-20 01:14:36 -0700

    remove redundant dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-eslint`],
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-react-next`],
 }
